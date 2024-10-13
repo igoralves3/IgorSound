f0511bcdb4a930c45aa65ab9143c6c3a0c59ff4d
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2024-10-13 10:57:47 -0300

    colocando cor e movimeno lateral conforme musica

--- IgorSound/Assets/Scripts/CubeSound.cs
+++ IgorSound/Assets/Scripts/CubeSound.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
@@ -9,14 +12,18 @@ namespace Deform{
 
         public AudioSource audioSource;
         private float[] spectrumData = new float[256];
+        private float[] samplesData = new float[256];
         private int dir = 1;
 
         public GameObject Twist;
+        public MeshRenderer meshRenderer;
 
         // Start is called before the first frame update
         void Start()
         {
             audioSource = GetComponent<AudioSource>();
+            meshRenderer = GetComponent<MeshRenderer>();
+            
             dir = 1;
         }
 
@@ -27,6 +34,12 @@ namespace Deform{
 
                 // Obtém os dados do espectro
                 audioSource.GetSpectrumData(spectrumData, 0, FFTWindow.Rectangular);
+
+
+                float lowFreq = GetAverage(spectrumData,0,80);
+                float mediumFreq = GetAverage(spectrumData,80,160);
+                float highFreq = GetAverage(spectrumData,160,256);
+
 
                 // Obtém a frequência máxima
                 float maxFreq = 0f;
@@ -44,7 +57,10 @@ namespace Deform{
 
                 Debug.Log(maxFreq);
 
-                Twist.GetComponent<TwistDeformer>().StartAngle += maxFreq * dir * Time.deltaTime;
+                var delta = maxFreq * dir * Time.deltaTime;
+
+                Twist.GetComponent<TwistDeformer>().StartAngle += delta;
+                this.transform.position += new Vector3(delta / 180, 0, 0);
                 if (Twist.GetComponent<TwistDeformer>().StartAngle >= 180)
                 {
                     dir = -1;
@@ -52,8 +68,29 @@ namespace Deform{
                 {
                     dir = 1;
                 }
+                if (lowFreq > mediumFreq && lowFreq > highFreq) {
+                    meshRenderer.material.SetColor("_Color", Color.red);
+                }else if (mediumFreq > lowFreq && mediumFreq > highFreq)
+                {
+                    meshRenderer.material.SetColor("_Color", Color.green);
+                }
+                else{
+                    meshRenderer.material.SetColor("_Color", Color.blue);
+                }
 
+                Debug.Log("Low: " + lowFreq + " Medium: " + mediumFreq + " High: " + highFreq);
             }
+        }
+
+        // Função auxiliar para calcular a média de uma faixa de frequências
+        float GetAverage(float[] data, int start, int end)
+        {
+            float sum = 0;
+            for (int i = start; i < end; i++)
+            {
+                sum += data[i];
+            }
+            return sum / (end - start);
         }
     }
 }
